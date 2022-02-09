fb2c98b27ebfe97f567c3a89c818b9f74c4cebd7
PauloMorillo <pauloan@hotmail.com>
2022-02-09 11:11:52 -0500

    [update] created curso model and register in admin.py

--- SanBuena/settings.py
+++ SanBuena/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tmvmq$pgm*aoyyz8hrqeb6gzv)v8(w8+4jojzgofiuon4)%)t#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'curso'
 ]
 
 MIDDLEWARE = [
--- curso/models.py
+++ curso/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Curso(models.Model):
+    name = models.CharField(max_length=50)
+    fecha_inicio = models.DateTimeField()
+    maximo_participantes = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} max: {self.maximo_participantes}'
